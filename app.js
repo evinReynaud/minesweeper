8086cb0e39e2b3cefaf503079f812fde685abbde
evinReynaud <44316037+evinReynaud@users.noreply.github.com>
2022-05-14 18:36:50 +0200

    Fix bug where you can't remove any flag when all are placed

--- app.js
+++ app.js
@@ -48,13 +48,15 @@ document.addEventListener('DOMContentLoaded', () => {
   //add Flag with right click
   function addFlag(square) {
     if (isGameOver) return
-    if (!square.classList.contains('checked') && (flags < bombAmount)) {
+    if (!square.classList.contains('checked')) {
       if (!square.classList.contains('flag')) {
-        square.classList.add('flag')
-        square.innerHTML = ' 🚩'
-        flags++
-        flagsLeft.innerHTML = bombAmount - flags
-        checkForWin()
+        if ((flags < bombAmount)) {
+          square.classList.add('flag')
+          square.innerHTML = ' 🚩'
+          flags++
+          flagsLeft.innerHTML = bombAmount - flags
+          checkForWin()
+        }
       } else {
         square.classList.remove('flag')
         square.innerHTML = ''
